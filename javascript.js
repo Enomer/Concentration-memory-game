f8186ffddd77291d891603770cdf24fd9b4573ca
AidzPrehot <aprehot@gmail.com>
2018-01-17 12:32:04 -0800

    *housekeeping *added max-width to images to not overflow there boxes *perfected RNG by keeping an i variable to track the classes assignments

--- javascript.js
+++ javascript.js
@@ -1,23 +1,12 @@
-function randomImg() {
-  let assigning = document.getElementsByClassName('assign');
 
-  /* one possible method for assigning images
+// the game randomly shuffles the cards
 
-  for (var i = 0; i < assigning.length; i++) {
-  if (assigning.img.src === "") {
-  let num = Math.floor(Math.random() * 8 + 1);
-  assigning[i].src = 'images/number' + num + '.gif';
-  assigning[i].alt = assigning[i].src;
-}
-}
-}*/
-
+let assigning = document.getElementsByClassName('assign');
 // the potential images
 let images = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
-
 // continue this until no more potential images
+var i = 0;
 while (images.length > 0) {
-
   // get a random index from the images array
   let index = Math.floor(Math.random() * images.length);
 
@@ -27,15 +16,6 @@ while (images.length > 0) {
 
   // remove that index from the array
 
-  images.splice(i,1);
-  console.log("gucci" + [i])
+  images.splice(index,1);
+  i++;
 }
-}
-
-let startBtn = document.querySelector('button');
-
-startBtn.addEventListener('click', function randomImg() {
-
-}
-
-);
