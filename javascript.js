6692822323a56921f6ddc1d557abad3052c4a371
AidzPrehot <aprehot@gmail.com>
2018-01-18 01:12:02 -0800

    added velocity and jquery temporarily for expedrimentation procedure *added boxes to each image and on click the class is removed to remove the boxes which reveals the rng shuffled gif below it

--- javascript.js
+++ javascript.js
@@ -9,13 +9,22 @@ var i = 0;
 while (images.length > 0) {
   // get a random index from the images array
   let index = Math.floor(Math.random() * images.length);
-
   //assign that image to assigning array
   assigning[i].src = 'images/number' + images[index] + '.gif';
   assigning[i].alt = assigning[i].src;
-
   // remove that index from the array
-
   images.splice(index,1);
   i++;
+}
+
+let imageTags = document.querySelector('img');
+let numOfGreys = document.querySelectorAll('#gameboard .grey').length;
+let remover = document.querySelectorAll('.grey');
+let assigned = document.querySelector('.assign');
+
+
+for (let i = 0; i < remover.length; i++) {
+  remover[i].addEventListener('click', function () {
+      this.classList.remove('grey');
+  });
 }
